9af974226108fd5e9f9bddd06c6a3fccdc96f36a
Renan <renan.aragorn@gmail.com>
2022-09-17 21:32:13 -0300

    game and ads table migrations done

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,33 @@
-const { response } = require("express")
+import { Request, Response } from "express"
+
 const express = require("express")
 
 const app = express()
 
-app.get('/ads', (request: any, response: { json: (arg0: { id: number; name: string }[]) => any }) => {
+app.get('/games', (request: Request, response: Response) => {
+  return response.json([])
+})
+
+app.post('/ads', (request: Request, response: Response) => {
+  return response.status(201).json([])
+})
+
+app.get('/games/:id/ads', (request: Request, response: Response) => {
+  //const gameId = request.params.id
+
   return response.json([
     { id:1, name:"Anúncio 1"},
     { id:2, name:"Anúncio 2"},
     { id:3, name:"Anúncio 3"},
     { id:4, name:"Anúncio 4"},
-    { id:4, name:"Anúncio 5"},
+    { id:5, name:"Anúncio 5"},
   ])
+})
+
+app.get('/games/:id/discord', (request: Request, response: Response) => {
+  //const gameId = request.params.discord
+
+  return response.json([])
 })
 
 app.listen(3333)
